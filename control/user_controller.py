61419e44a94434e17cc248539689a767d3bd14a8
马军 <253495832@qq.com>
2026-01-07 13:35:31 +0800

    改注释

--- control/user_controller.py
+++ control/user_controller.py
@@ -9,31 +9,34 @@ from service.user_service import UserService
 router = APIRouterPro(prefix="/users", tags=["users"], order_num=100)
 
 
-@router.post("/", response_model=DataResponseModel[User])
+@router.post("/", summary='创建新用户接口',
+             description='用于创建新用户', response_model=DataResponseModel[User])
 def create_user_endpoint(user: UserCreate, session: Session = Depends(get_session)):
-    """创建新用户"""
     return UserService.create_user(user, session)
 
 
-@router.get("/", response_model=DataResponseModel[list[User]])
+@router.get("/", summary='获取所有用户接口',
+            description='用于获取所有用户', response_model=DataResponseModel[list[User]])
 def list_users_endpoint(session: Session = Depends(get_session)):
-    """获取所有用户"""
     return UserService.list_users(session)
 
 
+@router.get("/{user_id}", summary='获取指定用户接口',
+            description='用于获取指定用户', response_model=DataResponseModel[User])
 @router.get("/{user_id}", response_model=DataResponseModel[User])
 def get_user_endpoint(user_id: int, session: Session = Depends(get_session)):
-    """获取指定用户"""
     return UserService.get_user(user_id, session)
 
 
+@router.put("/{user_id}", summary='更新用户信息接口',
+            description='用于更新用户信息', response_model=DataResponseModel[User])
 @router.put("/{user_id}", response_model=DataResponseModel[User])
 def update_user_endpoint(user_id: int, user: UserCreate, session: Session = Depends(get_session)):
-    """更新用户信息"""
     return UserService.update_user(user_id, user, session)
 
 
+@router.delete("/{user_id}", summary='删除用户接口',
+               description='用于删除用户', response_model=CrudResponseModel)
 @router.delete("/{user_id}", response_model=CrudResponseModel)
 def delete_user_endpoint(user_id: int, session: Session = Depends(get_session)):
-    """删除用户"""
     return UserService.delete_user(user_id, session)
